f81658a76b7b3806f0a52866f728647e801b4e20
IanaNeri22 <iananeri@outlook.com>
2025-07-20 11:06:59 -0300

    Fix steps do pagamento_repository

--- features/steps/pagamento_repository_steps.py
+++ features/steps/pagamento_repository_steps.py
@@ -16,6 +16,7 @@ def step_given_pagamento_valido(context):
         data_criacao=datetime.now()
     )
 
+
 @given("um pagamento com ID 123456 existe no banco")
 def step_given_pagamento_existe(context):
     context.pagamento_id = 123456
@@ -26,22 +27,24 @@ def step_given_pagamento_existe(context):
         "valor": "150.00",
         "data_criacao": datetime.now().isoformat()
     }
-    context.dynamo_patch = patch("app.domain.repositories.pagamento_repository.boto3.resource")
-    context.mock_boto = context.dynamo_patch.start()
-    mock_table = MagicMock()
-    mock_table.get_item.return_value = {"Item": context.pagamento_mock}
-    context.mock_boto.return_value.Table.return_value = mock_table
-    context.repo = PagamentoRepository()
+
+    with patch("app.domain.repositories.pagamento_repository.boto3.resource") as mock_boto:
+        mock_table = MagicMock()
+        mock_table.get_item.return_value = {"Item": context.pagamento_mock}
+        mock_boto.return_value.Table.return_value = mock_table
+        context.repo = PagamentoRepository()
+
 
 @given("nenhum pagamento com ID 999999 existe no banco")
 def step_given_pagamento_inexistente(context):
     context.pagamento_id = 999999
-    context.dynamo_patch = patch("app.domain.repositories.pagamento_repository.boto3.resource")
-    context.mock_boto = context.dynamo_patch.start()
-    mock_table = MagicMock()
-    mock_table.get_item.return_value = {}  # sem Item
-    context.mock_boto.return_value.Table.return_value = mock_table
-    context.repo = PagamentoRepository()
+
+    with patch("app.domain.repositories.pagamento_repository.boto3.resource") as mock_boto:
+        mock_table = MagicMock()
+        mock_table.get_item.return_value = {}  # sem Item
+        mock_boto.return_value.Table.return_value = mock_table
+        context.repo = PagamentoRepository()
+
 
 @when("eu salvar o pagamento")
 def step_when_salvar_pagamento(context):
@@ -52,10 +55,11 @@ def step_when_salvar_pagamento(context):
         context.repo = PagamentoRepository()
         context.resultado = context.repo.save(context.pagamento)
 
+
 @when("eu buscar o pagamento pelo ID")
 def step_when_buscar_pagamento(context):
     context.resultado = context.repo.get_pagamento_by_id(context.pagamento_id)
-    context.dynamo_patch.stop()
+
 
 @when("eu atualizar o pagamento")
 def step_when_atualizar_pagamento(context):
@@ -66,17 +70,20 @@ def step_when_atualizar_pagamento(context):
         context.repo = PagamentoRepository()
         context.resultado = context.repo.update_pagamento(context.pagamento)
 
+
 @then("o pagamento deve ser retornado com sucesso")
 @then("o pagamento deve ser atualizado com sucesso")
 def step_then_pagamento_retornado(context):
     assert context.resultado is not None
     assert isinstance(context.resultado, Pagamento)
 
+
 @then("o pagamento deve ser retornado")
 def step_then_pagamento_encontrado(context):
     assert context.resultado is not None
     assert isinstance(context.resultado, Pagamento)
     assert context.resultado.id_pagamento == 123456
+
 
 @then("nenhum pagamento deve ser encontrado")
 def step_then_pagamento_nao_encontrado(context):
