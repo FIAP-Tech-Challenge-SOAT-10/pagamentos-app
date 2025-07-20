7121a21ba53189f4fb1a708f70979e4cfc56a4ab
IanaNeri22 <iananeri@outlook.com>
2025-07-20 11:16:28 -0300

    Adicionado mock do dynamo no environment

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,15 @@
+from unittest.mock import MagicMock
+from requests import patch
 from fastapi.testclient import TestClient
 from app.lambda_function import app  # ou o caminho real da sua instância FastAPI
 
 
 def before_all(context):
     context.client = TestClient(app)
+    context.dynamo_patch = patch("app.domain.repositories.pagamento_repository.boto3.resource")
+    mock_dynamodb = MagicMock()
+    mock_dynamodb.Table.return_value = MagicMock()
+    context.dynamo_patch.start().return_value = mock_dynamodb
 
 def before_scenario(context, scenario):
     print(f"🚀 Iniciando cenário: {scenario.name}")
@@ -11,3 +17,6 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     print(f"✅ Finalizado cenário: {scenario.name}")
 
+def after_all(context):
+    context.dynamo_patch.stop()
+    print("🔚 Testes concluídos, DynamoDB mock finalizado.")
